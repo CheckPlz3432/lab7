2e74eb56e79de181f63774320195604fb0351016
Trent Coleman <69553545+CheckPlz3432@users.noreply.github.com>
2022-11-08 22:36:31 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 function init(){
 //add your javascrip between these two lines of code
-document.getElementByID("entrybutton").addEventListener("click", buttonRespondText);
-  function buttonRespondText() {
-    var inTextMsg = document.getElementById("entryinput").value;
-    var outTextMsg = document.getElementById("textoutput").innerHTML;
-    outTextMsg = inTextMsg;
-    alert("Trent Coleman: " + inTextMsg);
-  }
+  var button = document.getElementByID("entrybutton").value;
+  document.getElementByID("entrybutton").addEventListener("click", buttonRespondText);
+    function buttonRespondText() {
+      var inTextMsg = document.getElementById("entryinput").value;
+      document.getElementById("textoutput").innerHTML = inTextMsg;
+      alert("Trent Coleman: " + inTextMsg);
+    }
 }
 window.addEventListener("load", init);

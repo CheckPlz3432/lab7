5b6a03dd5a3a39baed54ba2ecf26b43fc5f7ebb2
Trent Coleman <69553545+CheckPlz3432@users.noreply.github.com>
2022-11-08 18:06:42 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 function init(){
 //add your javascrip between these two lines of code
-
+myEventFunction {
+ alert("Trent Coleman Hello this is a test");
+}
+var d4 = document.getElementById('div4');
+d4.addEventListener('click', myEventFunction);
+ 
+d4.addEventListener('click', Function(){
+  console.log('I was Clicked!');
+  d4.style.backgroundColor = '#C00';
+  d4.innerHTML = '<strong>Goodbye</strong>';
  
   alert("Trent Coleman Hello this is a test");
   
- 
+}
 window.addEventListener('load', init);
